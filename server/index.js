d74a2a9fa505e9eb7ee026893aa30b137c9c75f5
AsHim Shrestha <ashimsth89@gmail.com>
2024-10-08 10:28:39 +0545

    Refactor socket.io server configuration to allow CORS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const { Server } = require("socket.io");
 
-const io = new Server(4000);
+const io = new Server(4000, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("Socket connected", socket.id);
