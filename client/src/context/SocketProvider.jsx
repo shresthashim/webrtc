82c8bcf861ad7fa7a44552dead73664ce40c3902
AsHim Shrestha <ashimsth89@gmail.com>
2024-10-08 10:26:02 +0545

    Refactor main.jsx to include SocketProvider for socket.io integration

---
+++ client/src/context/SocketProvider.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useMemo } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  const context = useContext(SocketContext);
+  if (!context) {
+    throw new Error("useSocket must be used within a SocketProvider");
+  }
+  return context;
+};
+
+export const SocketProvider = ({ children }) => {
+  const socket = useMemo(() => io("http://localhost:4000"), []);
+
+  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
+import { SocketProvider } from "./context/SocketProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </Router>
   </StrictMode>
 );
