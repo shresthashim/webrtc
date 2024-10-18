82b1223275e87314b7e545ff1a6272c1622b398a
AsHim Shrestha <ashimsth89@gmail.com>
2024-10-18 21:43:11 +0545

    Add RoomPage component and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Lobby from "./pages/Lobby";
+import RoomPage from "./pages/Room";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Lobby />} />
+        <Route path='/room/:roomId' element={<RoomPage />} />
       </Routes>
     </>
   );
